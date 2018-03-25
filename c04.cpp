d0f44381fb724a7e01aa31af183c4208b6c73057
LeeSungje <sungje1025@naver.com>
2018-03-25 21:13:25 +0900

    18-03-25 21:13

--- c04.cpp
+++ c04.cpp
@@ -1,27 +1,12 @@
 #include <stdio.h>
-int main(void)
-{
-	int i=2, c=0, count=0;
-	
-	for(; i<=50; i++){
-		count = 0;
-		for(int k=1; k<=i && count<=2; k++){
-			if(i%k == 0) 
-				count++;
-		}
-		if(count == 2){
-			c++;
-			if(c%5 == 1)
-				printf("%d", i);
-
-			else if(c%5 == 0)
-				printf(" %d\n", i);
-		
-			else 
-				printf(" %d", i);
-		}
-	}
-
-	
+void func(int a){
+printf("func 함수가 실행되었습니다.\n");
+}
+int main(){
+	void (*pfunc)(int);
+	pfunc = func;
+	func(3);
+	pfunc(3);
 	return 0;
 }
+
